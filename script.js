b6d42eee8d2b8f9cd227aa856b6c1a304b85bed6
Daniel Komolafe <daniel@komolafe.dev>
2025-11-29 14:25:11 -0600

    finished task

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById("registration-form");
+  const feedbackDiv = document.getElementById("form-feedback");
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const username = document.getElementById('username').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+
+    let isValid = true;
+    let messages = [];
+
+    if (username.length < 3) {
+      isValid = false;
+      messages.push("Username cannot be less than 3 characters");
+    }
+
+    if (!email.includes('@') || !email.includes('.')) {
+      isValid = false;
+      messages.push("Enter a valid email");
+    }
+
+    if (password.length < 8) {
+      isValid = false;
+      messages.push("Password is too short");
+    }
+
+    feedbackDiv.style.display = "block";
+    if (isValid == true) {
+      feedbackDiv.textContent = "Registration successful!";
+      feedbackDiv.style.backgroundColor = "green";
+    }
+    else {
+      feedbackDiv.innerHTML = messages.join("<br>");
+    }
+
+  });
+})
